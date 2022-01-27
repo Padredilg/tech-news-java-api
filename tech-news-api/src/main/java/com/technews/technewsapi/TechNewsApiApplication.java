58975c6d8a5fe97521872e449b81033cb609f5c6
Padredilg <padredilg@knights.ucf.edu>
2022-01-27 11:36:35 -0500

    starting files

---
+++ tech-news-api/src/main/java/com/technews/technewsapi/TechNewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.technews.technewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsApiApplication.class, args);
+	}
+
+}
---
+++ tech-news-api/src/test/java/com/technews/technewsapi/TechNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technews.technewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
